28b0fe0b6ee804811bf8c59bf4ff42b3fec7331c
Amer Sharif <mr.amersharif@gmail.com>
2022-10-18 22:19:19 +0200

    feature: add WeatherType and WeatherData models and migrations

---
+++ app/Http/Controllers/WeatherTypeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\WeatherType;
+use Illuminate\Http\Request;
+
+class WeatherTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\WeatherType  $weatherType
+     * @return \Illuminate\Http\Response
+     */
+    public function show(WeatherType $weatherType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\WeatherType  $weatherType
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(WeatherType $weatherType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\WeatherType  $weatherType
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, WeatherType $weatherType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\WeatherType  $weatherType
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(WeatherType $weatherType)
+    {
+        //
+    }
+}
