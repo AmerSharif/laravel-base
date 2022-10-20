7aed18ba8748e3c0e68072305a958c62b884833b
Amer Sharif <mr.amersharif@gmail.com>
2022-10-20 07:42:13 +0200

    feature: add routes for weather endpoints

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,34 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group(['prefix' => 'v1', 'middleware' => 'auth:api'], function(){
+    Route::get('/user', function( Request $request ){
+      return $request->user();
+    });
+
+    /*
+    |-------------------------------------------------------------------------------
+    | Get All 
+    |-------------------------------------------------------------------------------
+    | URL:            /api/v1/LocationWeather
+    | Controller:     API\LocationWeatherController@getLocationWeather
+    | Method:         GET
+    | Description:    Gets weather for all locations
+    */
+    Route::get('/locationweather', 'API\LocationWeatherController@getLocationWeather');
+
+    /*
+    |-------------------------------------------------------------------------------
+    | Get weather for an individual location
+    |-------------------------------------------------------------------------------
+    | URL:            /api/v1/LocationWeather/{id}
+    | Controller:     API\LocationWeatherController@getLocationWeather
+    | Method:         GET
+    | Description:    Gets weather for an individual location
+    */
+    Route::get('/locationweather/{id}', 'API\LocationWeatherController@getCafe');
+});
+
 Route::fallback(function(){
     return response()->json([
         'message' => 'Page Not Found. If error persists, contact support'], 404);
